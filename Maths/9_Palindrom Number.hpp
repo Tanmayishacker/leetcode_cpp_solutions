4fb486e0788bb937f7de23afc4e83fa8312a9c7f
UP Gupta <ujjwal_11896@npssbs.org.in>
2024-12-22 17:35:02 +0000

    Added ollama in container and improved palindeom number.

--- Maths/9_Palindrom Number.hpp
+++ Maths/9_Palindrom Number.hpp
@@ -2,17 +2,15 @@
 
 bool isPalindrome(int64_t x)
 {
-    int64_t originalNumber = x;
-    int64_t reversedNumber = 0;
-
+    size_t originalNumber = x;
+    size_t reversedNumber = 0;
     // Reverse the number
     while (x > 0)
     {
-        int digit = x % 10;
-        reversedNumber = reversedNumber * 10 + digit;
+        int8_t digit = x % 10;
+        reversedNumber *= 10 + digit;
         x = x / 10;
     }
-
     // Check if the original number is equal to the reversed number
     return originalNumber == reversedNumber;
 }
